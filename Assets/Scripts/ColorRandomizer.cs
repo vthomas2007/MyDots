886e1c4ba83b8a6f2a63cd28c2c1fbd99ad766d0
Vince Thomas <vthomas2007@gmail.com>
2019-01-11 16:55:19 -0500

    Init

---
+++ Assets/Scripts/ColorRandomizer.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ColorRandomizer : MonoBehaviour {
+	public SpriteRenderer spriteRenderer;
+
+	static private Color YELLOW = new Color32(230, 219, 33,  255);
+	static private Color GREEN  = new Color32(140, 235, 148, 255);
+	static private Color BLUE   = new Color32(140, 190, 255, 255);
+	static private Color RED    = new Color32(239, 93,  66,  255);
+	static private Color PURPLE = new Color32(156, 93,  181, 255);
+
+	static public Color[] colors = { YELLOW, GREEN, BLUE, RED, PURPLE };
+	
+	void Start () {
+		int colorIndex = Random.Range(0, colors.Length);
+		spriteRenderer.color = colors[colorIndex];
+	}
+}
---
+++ Assets/Scripts/GridManager.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GridManager : MonoBehaviour {
+	private static int WIDTH = 6;
+	private static int HEIGHT = 6;
+	private GameObject[,] dots = new GameObject[WIDTH, HEIGHT];
+
+	public GameObject dotPrefab;
+
+	public float dotScale = 1.0f;
+	public float distanceBetweenDots = 1.0f;
+
+	// Use this for initialization
+	void Start () {
+		for (int i = 0; i < HEIGHT; i++) {
+			for (int j = 0; j < WIDTH; j++) {
+				dots[i,j] = Instantiate(dotPrefab, new Vector3((float)i * distanceBetweenDots, (float)j * distanceBetweenDots), Quaternion.identity);
+				dots[i,j].transform.localScale = new Vector2(dotScale, dotScale);
+			}
+		}
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
