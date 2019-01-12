0b3db50599e5699891974c7d319ca9c1306e572b
Vince Thomas <vthomas2007@gmail.com>
2019-01-12 17:36:21 -0500

    Connect the dots

--- Assets/Scripts/GridManager.cs
+++ Assets/Scripts/GridManager.cs
@@ -11,11 +11,20 @@ public class GridManager : MonoBehaviour {
 
 	public float dotScale = 1.0f;
 	public float distanceBetweenDots = 1.0f;
+	private float distanceBetweenDotsSquared;
+
+	// TODO Rename
+	private const float FUDGE_FACTOR = 0.05f;
+
+	public float lineWidth = 0.25f;
 
 	private List<GameObject> activeDots = new List<GameObject>();
 
 	// Use this for initialization
 	void Start () {
+		distanceBetweenDotsSquared = distanceBetweenDots * distanceBetweenDots + FUDGE_FACTOR;
+
+		// TODO Move to method
 		for (int i = 0; i < HEIGHT; i++) {
 			for (int j = 0; j < WIDTH; j++) {
 				dots[i,j] = Instantiate(dotPrefab, new Vector3((float)i * distanceBetweenDots, (float)j * distanceBetweenDots), Quaternion.identity);
@@ -31,21 +40,22 @@ public class GridManager : MonoBehaviour {
 		HandleMouseRelease();
 	}
 
+	public List<GameObject> GetActiveDots() {
+		return activeDots;
+	}
+
 	public GameObject GetCurrentDot() {
 		if (activeDots.Count > 0) {
 			return activeDots[activeDots.Count - 1];
 		}
 		return null;
 	}
-	// TODO: See if this can easily be moved to another class or even gameObject
+	// TODO: See if this can easily be moved to another class or component
 	private void HandleMouseClick() {
 		if (Input.GetMouseButtonDown(0)) {
-			Vector3 mousePosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
-			Vector2 mousePosition2D = new Vector2(mousePosition.x, mousePosition.y);
-			RaycastHit2D hit = Physics2D.Raycast(mousePosition2D, Vector2.zero);
-
-			if (hit.collider != null) {
-				activeDots.Add(hit.collider.gameObject);
+			Collider2D clickedDotCollider = GetColliderUnderMouseCursor();
+			if (clickedDotCollider != null) {
+				activeDots.Add(clickedDotCollider.gameObject);
 			}
 		}
 	}
@@ -55,11 +65,49 @@ public class GridManager : MonoBehaviour {
 		// Either move that into this class or move all the input handling
 		// into another class and find a clean way to pass information betwee
 		// all classes
+		if (Input.GetMouseButton(0)) {
+			Collider2D dotUnderCursorCollider = GetColliderUnderMouseCursor();
+
+			if (dotUnderCursorCollider != null) {
+				GameObject dotUnderCursor = dotUnderCursorCollider.gameObject;
+
+				if (activeDots.Count > 0) {
+					Vector3 offset = GetLastActiveDot().transform.position - dotUnderCursor.transform.position;
+					if (offset.sqrMagnitude < distanceBetweenDotsSquared && !InActiveSet(dotUnderCursor)) {
+						// TODO: Figure out how to fix this
+						if (GetLastActiveDot().GetComponent<SpriteRenderer>().color == dotUnderCursor.GetComponent<SpriteRenderer>().color) {
+							activeDots.Add(dotUnderCursor);
+						}
+					}
+				}
+			}
+		}
 	}
 
 	private void HandleMouseRelease() {
 		if (Input.GetMouseButtonUp(0)) {
 			activeDots.Clear();
 		}
+	}
+
+	private Collider2D GetColliderUnderMouseCursor() {
+		Vector3 mousePosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+		Vector2 mousePosition2D = new Vector2(mousePosition.x, mousePosition.y);
+		RaycastHit2D hit = Physics2D.Raycast(mousePosition2D, Vector2.zero);
+
+		return hit.collider;
+	}
+
+	private GameObject GetLastActiveDot() {
+		if (activeDots.Count > 0) {
+			return activeDots[activeDots.Count - 1];
+		}
+
+		return null;
+	}
+
+	// TODO: "ACtive" is confusing since Unity already uses the phrase, rename all of this
+	private bool InActiveSet(GameObject dot) {
+		return activeDots.Contains(dot);
 	}
 }
--- Assets/Scripts/LineUpdater.cs
+++ Assets/Scripts/LineUpdater.cs
@@ -1,5 +1,6 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
 using UnityEngine;
 
 public class LineUpdater : MonoBehaviour {
@@ -11,10 +12,13 @@ public class LineUpdater : MonoBehaviour {
 	private Vector3 start;
 	private Vector3 end;
 	
+	// TODO TODO TODO: Flip the dependency between the line renderer and the grid
+	// If the grid knows about the line renderer, it can just update/notify it when things change
+	// Alternative: Use events. But may not surface enough information about activedots etc.
 	void Start () {
 		lineRenderer = gameObject.AddComponent<LineRenderer>();
 		lineRenderer.material = new Material(Shader.Find("Sprites/Default"));
-		lineRenderer.widthMultiplier = 0.5f;
+		lineRenderer.widthMultiplier = 0.25f;
 
 		Gradient gradient = new Gradient();
 		gradient.SetKeys(
@@ -24,17 +28,32 @@ public class LineUpdater : MonoBehaviour {
 		lineRenderer.colorGradient = gradient;
 	}
 	
-	// Update is called once per frame
 	void Update () {
-		currentDot = gridManager.GetCurrentDot();
+		List<GameObject> activeDots = gridManager.GetActiveDots();
 
-		if (currentDot != null && Input.GetMouseButton(0)) {
+		if (Input.GetMouseButton(0) && activeDots.Count > 0) {
 			lineRenderer.enabled = true;
-			start = new Vector3(currentDot.transform.position.x, currentDot.transform.position.y, 0.0f );
+
+			// TODO: This obviously can't stick around
+			Color lineColor = activeDots[0].gameObject.GetComponent<SpriteRenderer>().color;
+			Gradient gradient = new Gradient();
+			gradient.SetKeys(
+				new GradientColorKey[] { new GradientColorKey(lineColor, 0.0f), new GradientColorKey(lineColor, 1.0f) },
+				new GradientAlphaKey[] { new GradientAlphaKey(1.0f, 0.0f), new GradientAlphaKey(1.0f, 1.0f) }
+			);
+			lineRenderer.colorGradient = gradient;
+			
+			List<Vector3> points = activeDots.Select((dot) => new Vector3(dot.transform.position.x, dot.transform.position.y, 0.0f)).ToList();
+			
 			end = Camera.main.ScreenToWorldPoint(Input.mousePosition);
 			end.z = 0;
-			Vector3[] points = new Vector3[] { start, end };
-			lineRenderer.SetPositions(points);
+
+			points.Add(end);
+
+			//Vector3[] points = new Vector3[] { start, end };
+			Debug.Log(points.Count);
+			lineRenderer.positionCount = points.Count;
+			lineRenderer.SetPositions(points.ToArray());
 		}
 		else {
 			lineRenderer.enabled = false;
