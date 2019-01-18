0838f9ead9fa8aa57fa6348e2c64670856a4f082
Vince Thomas <vthomas2007@gmail.com>
2019-01-18 07:55:02 -0500

    Add a few strategies

--- Assets/Scripts/DotColorStrategies/RandomSingleDotColorStrategy.cs
+++ Assets/Scripts/DotColorStrategies/RandomSingleDotColorStrategy.cs
@@ -2,19 +2,18 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class RandomDotColorStrategy : BaseDotColorStrategy {
+public class RandomSingleDotColorStrategy : BaseDotColorStrategy {
 	public override void AssignColors(GameObject[,] dots, Color[] colors) {
-		// TODO: Figure out how to handle/avoid all this boilerplate
 		int columns = dots.GetLength(0);
 		int rows = dots.GetLength(1);
 		int startingRow = rows / 2;
 
 		int colorCount = colors.Length;
+		Color c = colors[Random.Range(0, colorCount)];
 
 		for (int y = startingRow; y < rows; y++) {
 			for (int x = 0; x < columns; x++) {
 				if (dots[x, y] != null) {
-					Color c = colors[Random.Range(0, colorCount)];
 					dots[x, y].gameObject.GetComponent<SpriteRenderer>().color = c;
 				}
 			}
--- Assets/Scripts/DotColorStrategies/RotatingSingleColorStrategy.cs
+++ Assets/Scripts/DotColorStrategies/RotatingSingleColorStrategy.cs
@@ -2,22 +2,22 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class RandomDotColorStrategy : BaseDotColorStrategy {
+public class RotatingSingleColorStrategy : BaseDotColorStrategy {
+	private int colorIndex = 0;
+
 	public override void AssignColors(GameObject[,] dots, Color[] colors) {
-		// TODO: Figure out how to handle/avoid all this boilerplate
 		int columns = dots.GetLength(0);
 		int rows = dots.GetLength(1);
 		int startingRow = rows / 2;
 
-		int colorCount = colors.Length;
-
 		for (int y = startingRow; y < rows; y++) {
 			for (int x = 0; x < columns; x++) {
 				if (dots[x, y] != null) {
-					Color c = colors[Random.Range(0, colorCount)];
-					dots[x, y].gameObject.GetComponent<SpriteRenderer>().color = c;
+					dots[x, y].gameObject.GetComponent<SpriteRenderer>().color = colors[colorIndex];
 				}
 			}
 		}
+
+		colorIndex = (colorIndex + 1) % colors.Length;
 	}
 }
